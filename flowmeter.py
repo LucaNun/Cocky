a3c8acf33f4cb6d2f4015c32a176019172b1aa68
LucaNun <30501551+LucaNun@users.noreply.github.com>
2020-10-19 21:20:12 +0200

    19.10.2020 + Flowmeter Classe

---
+++ flowmeter.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+class Flowmeter():
+    def __init__(pin, ml):
+        self.pin = pin
+        self.ml = ml
+        self.imp = 0
+
+    def addsome(self, pinnumber):
+        self.imp += 1
+
+    def messen(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.pin, GPIO.IN)
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.addsome)
+
+        self.start = time.time()
+        while imp < 1000:
+            time.sleep(0.1)
+        self.end = time.time()
+
+        '''
+        HIER MUSS DER CODE ZUR BESTIMMUNG DER MENGE REIN
+        VLLT AUCH HÖHER
+        IMP / 73
+        ERGEBNIS * DAUER
+        = MENGE
+        '''
